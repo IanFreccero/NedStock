d41b0679adc2fada72ee4cd11ee1c01c9e9d5c9c
IanFreccero <ianfreccero@gmail.com>
2025-10-22 17:36:52 -0300

    Algunos cambios que tuve que hacer a la hora de deployear

--- server/src/index.js
+++ server/src/index.js
@@ -7,21 +7,40 @@ import salesRoutes from "./routes/sales.js";
 import salesSummaryRoutes from "./routes/salesSummary.js";
 import productSalesRoutes from "./routes/productSales.js";
 
-
 dotenv.config();
 const app = express();
-app.use(cors()); 
+
+const allowedOrigins = [
+  "https://neds-stock-client-9mo9.vercel.app",
+  "http://localhost:3000"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
 app.use(express.json());
 
 connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Servidor Express activo en Vercel 🚀");
+});
 
 app.use("/api/products", productRoutes);
 app.use("/api/sales", salesRoutes);
 app.use("/api/sales-summary", salesSummaryRoutes);
 app.use("/api/product-sales", productSalesRoutes);
 
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server Running on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Listening on port: ", process.env.PORT)
+})
